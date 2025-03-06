d1ccf86f25c6fefb6f0233bb21cb6eb9a04ef482
猫小九 <2935229453@qq.com>
2025-03-06 17:26:14 +0800

    修复button文档

--- docs/demos/button/button-05-size.vue
+++ docs/demos/button/button-05-size.vue
@@ -1,23 +1,25 @@
 <template>
-  <c-space wrap>
-    <c-button size="small" type="primary">主要按钮</c-button>
-    <c-button size="small" type="success">成功按钮</c-button>
-    <c-button ghost size="small" type="info">信息按钮</c-button>
-    <c-button ghost size="small" type="danger">危险按钮</c-button>
-    <c-button size="small" type="warning">警告按钮</c-button>
-  </c-space>
-  <c-space wrap>
-    <c-button type="primary">主要按钮</c-button>
-    <c-button type="success">成功按钮</c-button>
-    <c-button ghost type="info">信息按钮</c-button>
-    <c-button ghost type="danger">危险按钮</c-button>
-    <c-button type="warning">警告按钮</c-button>
-  </c-space>
-  <c-space wrap>
-    <c-button size="large" type="primary">主要按钮</c-button>
-    <c-button size="large" type="success">成功按钮</c-button>
-    <c-button ghost size="large" type="info">信息按钮</c-button>
-    <c-button ghost size="large" type="danger">危险按钮</c-button>
-    <c-button size="large" type="warning">警告按钮</c-button>
+  <c-space :size="10" direction="vertical">
+    <c-space wrap>
+      <c-button size="small" type="primary">主要按钮</c-button>
+      <c-button size="small" type="success">成功按钮</c-button>
+      <c-button ghost size="small" type="info">信息按钮</c-button>
+      <c-button ghost size="small" type="danger">危险按钮</c-button>
+      <c-button size="small" type="warning">警告按钮</c-button>
+    </c-space>
+    <c-space wrap>
+      <c-button type="primary">主要按钮</c-button>
+      <c-button type="success">成功按钮</c-button>
+      <c-button ghost type="info">信息按钮</c-button>
+      <c-button ghost type="danger">危险按钮</c-button>
+      <c-button type="warning">警告按钮</c-button>
+    </c-space>
+    <c-space wrap>
+      <c-button size="large" type="primary">主要按钮</c-button>
+      <c-button size="large" type="success">成功按钮</c-button>
+      <c-button ghost size="large" type="info">信息按钮</c-button>
+      <c-button ghost size="large" type="danger">危险按钮</c-button>
+      <c-button size="large" type="warning">警告按钮</c-button>
+    </c-space>
   </c-space>
 </template>
